ac5e75d9f62f5c400b970e0036d354ef05c4f27d
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-17 10:15:00 +0700

    update

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+
+// ... the starter code you pasted ...
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
