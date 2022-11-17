c62ab082748755a47650a36519a80a296356e017
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-17 10:47:07 +0700

    update

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const fname = "Mark Zuckaberg"
 function Hello() {
     return(
         <div>
-            <h1>Welcome to ReactJS!</h1>
+            <h1 className="text">Welcome to ReactJS!</h1>
             <hr />
-            <h1>Hello, World! {fname}</h1>
+            <h1 className="text2">Hello, World! {fname}</h1>
         </div>
     ) 
 }
+
+
 // const domContainer = document.querySelector('#root');
 const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
