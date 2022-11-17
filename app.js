1b8419ae53449c3d1d4a759d447a1933a2c09e56
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-17 11:26:17 +0700

    update

--- app.js
+++ app.js
@@ -1,19 +1,67 @@
 //1.Assignment variable
 const fname = "Mark Zuckaberg"
 
-//2.Function
-function Hello() {
+//2.Function Componet
+function Greeting() {
+    return(
+        <div>
+            <h1>Hello Reactjs!</h1>
+        </div>
+    ) 
+}
+
+function Frontend(){
+    return(
+        <div>
+            <h2>Client side rendering</h2>
+            <ol type="A">
+                <li>ReactJS</li>
+                <li>VueJS</li>
+                <li>Aungular</li>
+            </ol>
+        </div>
+    )
+}
+
+function Backend(){
+    return(
+        <div>
+        <h2>Server side rendering</h2>
+        <ol type="I">
+            <li>NextJS</li>
+            <li>NuxtJS</li>
+            <li>NestJS</li>
+        </ol>
+        </div>
+    )
+}
+
+function Time(){
+    const cTime = new Date().toTimeString()
+    return(
+        <div>
+            <p>The current time is{cTime}</p>
+        </div>
+    )
+}
+
+function MyApp(){
     return(
         <div>
             <h1 className="text">Welcome to ReactJS!</h1>
             <hr />
             <h1 className="text2">Hello, World! {fname}</h1>
+            <hr />
+            <Greeting />
+            <Frontend />
+            <Backend />
+            <Time />
         </div>
-    ) 
+    )
 }
 
 
 // const domContainer = document.querySelector('#root');
 const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Hello />);
+root.render(<MyApp />);
