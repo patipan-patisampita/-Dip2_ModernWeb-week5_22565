94f363104df9349c9072d13e2cb26f4d3ba96e39
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-17 10:39:59 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const fname ="Mark Zuckaberg"
+//1.Assignment variable
+const fname = "Mark Zuckaberg"
 
-
-const domContainer = document.querySelector('#root');
-const root = ReactDOM.createRoot(domContainer);
-root.render(fname);
+//2.Function
+function Hello() {
+    return(
+        <div>
+            <h1>Welcome to ReactJS!</h1>
+            <hr />
+            <h1>Hello, World! {fname}</h1>
+        </div>
+    ) 
+}
+// const domContainer = document.querySelector('#root');
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Hello />);
